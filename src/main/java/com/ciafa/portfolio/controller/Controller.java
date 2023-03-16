96cc1f00735c9eb36d27356859e1e8af86efdb77
C14F4 <electro.ciafa@gmail.com>
2023-03-16 20:01:22 -0300

    eliminacion de clase WebConfig y agregado de funcion de manejo de CORS

--- src/main/java/com/ciafa/portfolio/controller/Controller.java
+++ src/main/java/com/ciafa/portfolio/controller/Controller.java
@@ -4,15 +4,15 @@ import com.ciafa.portfolio.model.Perfil;
 import com.ciafa.portfolio.service.IPerfilService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin
 @RestController
+@CrossOrigin(origins = "/**")
 
 public class Controller {
     
