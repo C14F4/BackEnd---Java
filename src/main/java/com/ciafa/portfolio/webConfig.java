8dcaba4da733e4aa2c807c8049c472aae17ecdc1
C14F4 <electro.ciafa@gmail.com>
2023-03-16 18:31:34 -0300

    agregado de class WebConfig para manejo de CORS

--- src/main/java/com/ciafa/portfolio/webConfig.java
+++ src/main/java/com/ciafa/portfolio/webConfig.java
@@ -14,7 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class webConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("/**");
+        registry.addMapping("/**").allowedOrigins("*");
 
     }           
 
