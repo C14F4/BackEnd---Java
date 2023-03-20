b3334601cc03573ed766bee931a0b8bb125ddf43
C14F4 <electro.ciafa@gmail.com>
2023-03-20 08:18:01 -0300

    modificacion de clase WebConfig de manejo de CORS

---
+++ src/main/java/com/ciafa/portfolio/WebConfig.java
@@ -0,0 +1,26 @@
+package com.ciafa.portfolio;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Clase WebConfig para manejo de acceso desde origenes cruzados
+ * @author C14F4
+ */
+
+@Configuration
+@EnableWebMvc
+
+public class WebConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
